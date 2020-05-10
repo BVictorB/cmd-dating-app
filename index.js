61c9a048d1e09925e924f64dcc755b1ec51a3ef9
bvictorb <victory-boucher@hotmail.com>
2020-05-10 23:21:53 +0200

    added express and basic express setup

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const camelCase = require('camelcase');
+const express = require('express');
 
 console.log(camelCase('this-is-camelcase'));
+
+app.use('/static', express.static('public'));
+
+express()
+    .get('/', onhome)
+    .listen(1900);
+
+function onhome(req, res) {
+    res.send('<h1>TEST!</h1>\n');
+}

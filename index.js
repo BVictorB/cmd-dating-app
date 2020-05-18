b3f978f6f447afd50480aa8f16120f2611db02c7
bvictorb <victory-boucher@hotmail.com>
2020-05-18 15:52:40 +0200

    added handlebars and sass, made simple handlebars, express and sass setup

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const camelCase = require('camelcase');
-console.log(camelCase('this-is-camelcase'));
-
 const express = require('express');
+const hbs = require('express-handlebars');
 const app = express();
+
 const port = 1900;
-
-app.use('/static', express.static('static'));
-
-app.get('/', home);
-
-function home(req, res) {
-    res.send('<h1>TEST!</h1>\n');
-}
 
 app.listen(port, function() {
     console.log('Server running!');
 });
 
-app.get('/file', function(req, res) {
-    res.sendfile('file/examplefile.txt');
-});
+app.engine('hbs', hbs({ extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
-app.get('/mp3', function(req, res) {
-    res.sendfile('file/examplemp3.mp3');
-});
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.render('partials/login/login.hbs');
+} )
